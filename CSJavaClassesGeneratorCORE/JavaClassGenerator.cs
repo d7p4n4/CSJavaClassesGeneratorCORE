1f3c75a940efff9c3c903162959a99c893ddb46f
d7p4n4 <bercikezs199999999@gmail.com>
2020-05-19 12:42:17 +0200

    Add project files.

---
+++ CSJavaClassesGeneratorCORE/JavaClassGenerator.cs
@@ -0,0 +1,139 @@
+﻿using Ac4yClassModule.Class;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace CSJavaClassesGeneratorCORE
+{
+    class JavaClassGenerator
+    {
+
+        #region members
+
+        public string TemplatePath { get; set; }
+        public string TemplateSubPath { get; set; }
+        public string OutputPath { get; set; }
+        public string ProjectName { get; set; }
+
+        public Ac4yClass Ac4yClass { get; set; }
+
+        private const string TemplateExtension = ".csT";
+
+        private const string Suffix = "BaseClass";
+
+        private const string ClassCodeMask = "#classCode#";
+        private const string SuffixMask = "#suffix#";
+        private const string PackageNameMask = "#packageName#";
+        private const string InheritanceMask = "#inheritance#";
+        private const string PropertyTypeMask = "#type#";
+        private const string PropertyNameMask = "#propertyName#";
+        private const string PropertyInfoMask = "#propertyInfo#";
+
+        private const string ClassCodeAsVariableMask = "#classCodeAsVariable#";
+
+        #endregion members
+
+        public string ReadIntoString(string fileName)
+        {
+
+            string textFile = TemplatePath + TemplateSubPath + fileName + TemplateExtension;
+
+            return File.ReadAllText(textFile);
+
+        } // ReadIntoString
+
+        public void WriteOut(string text, string fileName, string outputPath)
+        {
+            File.WriteAllText(outputPath + fileName + ".java", text);
+
+        }
+
+        public string GetNameWithLowerFirstLetter(String Code)
+        {
+            return
+                char.ToLower(Code[0])
+                + Code.Substring(1)
+                ;
+
+        } // GetNameWithLowerFirstLetter
+
+        public string GetHead()
+        {
+
+            return ReadIntoString("Head")
+                        .Replace(ClassCodeMask, Ac4yClass.Name)
+                        .Replace(InheritanceMask, Ac4yClass.Ancestor.Equals(null) ? "extends " + Ac4yClass.Ancestor : "")
+                        .Replace(PackageNameMask, ProjectName)
+                        ;
+
+        }
+
+        public string GetFoot()
+        {
+            return
+                ReadIntoString("Foot")
+                        .Replace(ClassCodeMask, Ac4yClass.Name)
+                        .Replace(SuffixMask, Suffix)
+                        ;
+
+        }
+
+        public string GetMethods()
+        {
+            return
+                ReadIntoString("Methods")
+                ;
+        }
+
+        public string GetProperties(Ac4yProperty ac4yProperty)
+        {
+            return
+                ReadIntoString("Properties")
+                        .Replace(PropertyNameMask, ac4yProperty.Name)
+                        .Replace(PropertyTypeMask, ac4yProperty.Type.Equals("Int32") ? "Integer" : ac4yProperty.Type)
+                ;
+        }
+
+        public JavaClassGenerator Generate()
+        {
+
+            string result = null;
+
+            result += GetHead();
+
+            if(Ac4yClass.PropertyList.Count > 0)
+            {
+                for(int propertyNumber = 0; propertyNumber < Ac4yClass.PropertyList.Count; propertyNumber++)
+                {
+                    result += GetProperties(Ac4yClass.PropertyList[propertyNumber]);
+                }
+            }
+
+            result += GetMethods();
+
+            result += GetFoot();
+
+            WriteOut(result, Ac4yClass.Name, OutputPath);
+
+            return this;
+
+        } // Generate
+
+        public JavaClassGenerator Generate(Ac4yClass ac4yClass)
+        {
+
+            Ac4yClass = ac4yClass;
+
+            return Generate();
+
+        } // Generate
+
+        public void Dispose()
+        {
+            //throw new NotImplementedException();
+        }
+
+    }
+
+} // JavaClassGenerator
---
+++ CSJavaClassesGeneratorCORE/Program.cs
@@ -0,0 +1,94 @@
+﻿using Ac4yClassModule.Class;
+using Ac4yClassModule.Service;
+using log4net;
+using log4net.Config;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+
+namespace CSJavaClassesGeneratorCORE
+{
+    class Program
+    {
+
+
+        #region members
+
+        private static readonly ILog log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+        private const string APPSETTINGS_OUTPUTPATH = "OUTPUTPATH";
+
+        private const string APPSETTINGS_JAVAOUTPUTPATH = "JAVAOUTPUTPATH";
+        private const string APPSETTINGS_TEMPLATEPATH = "TEMPLATEPATH";
+
+        private const string APPSETTINGS_TEMPLATESUBPATH = "TEMPLATESUBPATH";
+
+        private const string APPSETTINGS_JAVATEMPLATESUBPATH = "JAVATEMPLATESUBPATH";
+
+        private const string APPSETTINGS_PROJECTNAME = "PROJECTNAME";
+
+        public IConfiguration Config { get; set; }
+
+        #endregion members
+
+
+        public Program(IConfiguration config)
+        {
+
+            Config = config;
+
+        } // Program
+
+        public void Run()
+        {
+
+            Ac4yClass ac4yClass = new Ac4yClassHandler().GetAc4yClassFromType(typeof(CSEFTPC4Core3Objects.Ac4yObjects.Ac4yPersistentChild));
+
+            new JavaClassGenerator()
+            {
+                TemplatePath = Config[APPSETTINGS_TEMPLATEPATH]
+                ,
+                TemplateSubPath = Config[APPSETTINGS_JAVATEMPLATESUBPATH]
+                ,
+                OutputPath = Config[APPSETTINGS_JAVAOUTPUTPATH]
+                ,
+                ProjectName = Config[APPSETTINGS_PROJECTNAME]
+            }
+                .Generate(ac4yClass);
+
+
+        } // run
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                var logRepository = LogManager.GetRepository(Assembly.GetEntryAssembly());
+                XmlConfigurator.Configure(logRepository, new FileInfo("log4net.config"));
+
+                IConfiguration config = null;
+
+                config = new ConfigurationBuilder()
+                            .AddJsonFile("appsettings.json", true, true)
+                            .Build();
+
+                Console.WriteLine(config[APPSETTINGS_JAVAOUTPUTPATH]);
+
+                new Program(config).Run();
+
+            }
+            catch (Exception exception)
+            {
+
+                log.Error(exception.Message);
+                log.Error(exception.StackTrace);
+
+                Console.ReadLine();
+
+            }
+
+        } // Main
+    }
+}
